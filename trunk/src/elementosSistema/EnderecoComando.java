bc6d5766098038498564c4ee5a0e686e7f7ab178
marcelo011982 <marcelo011982@21109577-f355-0410-9da5-4b3d9b81b5f8>
2008-09-05 05:33:40 +0000

    Com a memória imprimindo o endereço dos comandos na memória virtual.

--- trunk/src/elementosSistema/EnderecoComando.java
+++ trunk/src/elementosSistema/EnderecoComando.java
@@ -2,32 +2,39 @@ package elementosSistema;
 
 public class EnderecoComando {
 	//pega qual é o endereço
-	String prefixo;
-	String sufixo;
-	String endereco;
-	public EnderecoComando(String endereco) {				
-		//this.prefixo = String.valueOf(pref);
-		//this.sufixo = String.valueOf(suf);
+	
+	String idProcedimento;
+	String idPagina;
+	String ordem;
+	
+	public String getIdProcedimento() {
+		return idProcedimento;
 	}
-	public String getPrefixo() {
-		return prefixo;
-	}
-	public void setPrefixo(String prefixo) {
-		this.prefixo = prefixo;
-	}
-	public String getSufixo() {
-		return sufixo;
-	}
-	public void setSufixo(String sufixo) {
-		this.sufixo = sufixo;
-	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
+
+	public void setIdProcedimento(String idProcedimento) {
+		this.idProcedimento = idProcedimento;
 	}
 	
+	public String getIdPagina() {
+		return idPagina;
+	}
+	public void setIdPagina(String idPagina) {
+		this.idPagina = idPagina;
+	}
+	public String getOrdem() {
+		return ordem;
+	}
+	public void setOrdem(String ordem) {
+		this.ordem = ordem;
+	}
+	
+	public String getEndereco(){
+		String retorno;
+		retorno = this.getIdProcedimento()+this.getIdPagina()+this.getOrdem();
+		return retorno;
+	}
+	public EnderecoComando() {				
+	}
 	
 	
 		
--- trunk/src/kernel/Processador.java
+++ trunk/src/kernel/Processador.java
@@ -1,12 +1,19 @@
-package elementosSistema;
+package kernel;
+
+import elementosSistema.Comando;
+
 /***
  *  
  * @author Marcelo Iwata
  * Altera os comandos , para que um a um seja executado.
  */
 public class Processador {
+	int ordem;
+	int idProcesso;
 	//Construtor, pega apenas o id da página a ser utilizada
-	public Processador(int id){		
+	public Processador(int idProcesso){
+		this.idProcesso =idProcesso;
+		this.ordem = 0;
 	}
 	
 	//verifica se, na memória virtual , tem um comando
@@ -15,6 +22,7 @@ public class Processador {
 	}
 	//processa um comando, caso este exista no processo
 	private boolean Processa(){
+		this.ordem++;
 		return true;
 	}
 	
